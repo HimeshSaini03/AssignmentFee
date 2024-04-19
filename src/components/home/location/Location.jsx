936990c4fe7f62f60446f52e58b6720fd81b9dc8
HimeshSaini03 <122435858+HimeshSaini03@users.noreply.github.com>
2024-04-19 21:34:11 +0530

    Add files via upload

---
+++ src/components/home/location/Location.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Heading from "../../common/Heading"
+import { location } from "../../data/Data"
+import "./style.css"
+
+const Location = () => {
+  
+}
+
+export default Location
---
+++ src/components/home/team/Team.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Heading from "../../common/Heading"
+import { team } from "../../data/Data"
+import "./team.css"
+
+const Team = () => {
+  
+}
+
+export default Team
